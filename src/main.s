67c4e9a300b3a4ed78f8e03cc3001657c7d0d0e7
flixytss <flixytssyutu@gmail.com>
2025-08-29 17:05:00 -0400

    bringing malloc again but with it work different

--- src/main.s
+++ src/main.s
@@ -3,15 +3,17 @@ section .data
 _alloc: dq 0
 
 section .bss
-global MEMORY
+
 MEMORY: resb 1024
 
 section .text
 _start:
-    mov rsi, 0x3
-    call put
-    mov rsi, rax
-    call copy
+    mov rsi, 4
+    call malloc
+    mov rsi, 10
+    call malloc
+
+    call freeall
 
     jmp $
 
@@ -23,10 +25,34 @@ copy:
     mov rsi, [rbx]
     call put
     ret
-    
+malloc:
+    .loop:
+        cmp rbx, rsi
+        je .done
+        push rsi
+        inc rbx
+        mov rsi, rbx
+        call put
+        pop rsi
+        jmp .loop
+    .done:
+        xor rbx, rbx
+        sub rax, rsi
+        ret
 put:
     mov rdx, [_alloc]
     mov [MEMORY+rdx], rsi
     lea rax, [MEMORY+rdx]
     inc byte [_alloc]
     ret
+freeall:
+    xor rsi, rsi
+    .loop:
+        cmp rsi, [_alloc]
+        je .done
+        mov byte [MEMORY+rsi], 0x00
+        inc rsi
+        jmp .loop
+    .done:
+        mov byte [_alloc], 0
+        ret
